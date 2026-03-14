bf1777e556094f3ab55b99c8961f61dffba47f22
johnsaboyafulca-lab <johnsaboyafulca@gmail.com>
2026-03-14 14:59:44 -0500

    agregue los permisos de la ruta backend

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*",
     "http://localhost:5173",
-    "",
+    "https://prest-app-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
