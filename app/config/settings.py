78ad4e50aba260c9111085cd6cc2fc44fb174c5d
JOHN  SABOYA FULCA <afheryita@gmail.com>
2026-04-12 18:48:08 -0500

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://prest-app-frontend.vercel.app",
     ]
 
     # Entorno
